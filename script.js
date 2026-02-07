eae6a8c3a1352a808dcd2258452eaac5d8f5ac63
Saniyakhan10 <khansaniya54700@gmail.com>
2026-02-08 00:37:29 +0530

    first commit change

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let myform = document.getElementById("myform");
-let input = document.getElementById("input");
-let btn = document.getElementById("btn");
-let msg = document.getElementById("msg");
+ let myform = document.getElementById("myform");
+ let input = document.getElementById("input");
+ let msg = document.getElementById("msg");
+ let btn = document.getElementById("btn");
 
-myform.addEventListener("submit", function(e) {
-    e.preventDefault();
 
-    let name = input.value;
+ myform.addEventListener("submit", function(e) {
+     e.preventDefault();
 
-    localStorage.setItem("username", name);
-    msg.innerText = name;
-});
+     let name = input.value;
+     localStorage.setItem("username", name);
+     msg.innerText = name;
+ });
 
-let saveName = localStorage.getItem("username");
-if (saveName) {
-    msg.innerText = saveName;
-};
 
-btn.addEventListener("click", function() {
-    msg.innerText = "";
-})
+ let saveName = localStorage.getItem("username");
+ if (saveName) {
+     msg.innerText = saveName;
+ }
+
+ btn.addEventListener("click", function() {
+     msg.innerText = "";
+ });
